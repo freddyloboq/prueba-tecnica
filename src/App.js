5de383f702a5c51cf6e994c964ffbd7ee4567cd8
Freddy Lobo <freddyloboq@gmail.com>
2022-03-17 19:50:21 -0300

    Prueba técnica

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import Tatooine from "./img/file.jpg";
 
 function App() {
+  const [people, setPeople] = useState([]);
+  const [planet, setPlanet] = useState([]);
+
+  useEffect(() => {
+    fetch("https://swapi.dev/api/people/1")
+      .then((res) => res.json())
+      .then((data) => setPeople(data));
+
+    fetch("https://swapi.dev/api/planets/1")
+      .then((res) => res.json())
+      .then((data) => setPlanet(data));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+      <h1 id="title">StarWar</h1>
+      <div className="container">
+        <p className="subTitle">
+          Este es el planeta de{" "}
+          <span className="fontStrong">{people.name}</span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <div className="card">
+        <div className="container-text">
+        <div>
+          <h4>Nombre del planeta: {planet.name}</h4>
+          <p>Diametro del planeta: {planet.diameter}</p>
+          <p>Orbita del planeta: {planet.orbital_period}</p>
+          <p>Población del planeta: {planet.population}</p>
+        </div>
+            <img src={Tatooine} alt="" className="imagen" />
+        </div>
+        </div>
+      </div>
+    </>
   );
 }
 
